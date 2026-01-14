a6a67735821716950c5393771ad2657f5c947d86
GiriAdam78 <giridiwa6@gmail.com>
2026-01-15 00:11:32 +0700

    Versi Pertama

---
+++ apps/docs/app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <header className="sticky top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/60 dark:border-border">
+            <div className="flex h-16 items-center justify-between gap-4 container">
+                <a className="flex items-center gap-2 uppercase font-sans font-semibold">🌙 Yoru UI</a>
+            </div>
+        </header>
+    )
+}
---
+++ packages/registry/index.ts
@@ -0,0 +1,14 @@
+export const registry = {
+    button:{
+        files:[
+            {
+                from :"packages/ui/button.tsx",
+                to:"components/ui/button.tsx"
+            },
+        ],
+        depedencies:[
+            "@base-ui/react",
+            "clsx"
+        ]
+    }
+}
